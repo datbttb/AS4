3443e05185aecba9482ade2798d1d3b73ab8064e
datbttb <nguyendinhdat23082001@gmail.com>
2023-03-02 01:20:19 +0700

    first commit

---
+++ ecomstore/cart/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.template import RequestContext
+import cart
+import random
+
+
+def show_cart(request, template_name="cart/cart.html"):
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+        if postdata['submit'] == 'Remove':
+            cart.remove_from_cart(request)
+        if postdata['submit'] == 'Update':
+            cart.update_cart(request)
+    cart_items = cart.get_cart_items(request)
+    page_title = 'Shopping Cart'
+    cart_subtotal = cart.cart_subtotal(request)
+    return render(request, template_name, context_instance=RequestContext(request))
---
+++ ecomstore/catalog/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext
+
+from catalog.models import Category, Product
+from django.urls import reverse
+from cart import cart
+from django.http import HttpResponseRedirect
+from catalog.forms import ProductAddToCartForm
+
+
+# Create your views here.
+def catalog_home(request):
+    return render(request, 'catalog/index.html',
+                  context={'page_title': 'Musical Instruments and Sheet Music for Musicians'})
+
+
+def show_category(request, category_slug):
+    c = get_object_or_404(Category, slug=category_slug)
+    products = c.product_set.all()
+    page_title = c.name
+    meta_keywords = c.meta_keywords
+    meta_description = c.meta_description
+    context = {
+        'c': c,
+        'products': products,
+        'page_title': page_title,
+        'meta_keywords': meta_keywords,
+        'meta_description': meta_description,
+    }
+    return render(request, 'catalog/category.html', context)
+
+
+def show_product(request, product_slug):
+    p = get_object_or_404(Product, slug=product_slug)
+    categories = p.categories.filter(is_active=True)
+    page_title = p.name
+    meta_keywords = p.meta_keywords
+    meta_description = p.meta_description
+    context = {
+        'p': p,
+        'categories': categories,
+        'page_title': page_title,
+        'meta_keywords': meta_keywords,
+        'meta_description': meta_description,
+    }
+    return render(request, 'catalog/product.html', context)
+
+
+def show_product(request, product_slug, template_name="catalog/product.html"):
+    p = get_object_or_404(Product, slug=product_slug)
+    categories = p.categories.all()
+    page_title = p.name
+    meta_keywords = p.meta_keywords
+    meta_description = p.meta_description
+    # need to evaluate the HTTP method
+    if request.method == 'POST':
+        # add to cart...create the bound form
+        postdata = request.POST.copy()
+        form = ProductAddToCartForm(request, postdata)
+        # check if posted data is valid
+        if form.is_valid():
+            cart.add_to_cart(request)
+            # if test cookie worked, get rid of it
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
+            url =reverse('show_cart')
+            return HttpResponseRedirect(url)
+    else:
+        form = ProductAddToCartForm(request=request, label_suffix=':')
+        # assign the hidden input the product slug
+        form.fields['product_slug'].widget.attrs['value'] = product_slug
+        # set the test cookie on our first GET request
+        request.session.set_test_cookie()
+        return render(request, template_name, context_instance=RequestContext(request))
